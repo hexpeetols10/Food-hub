b81cf839cc6a04603793f61cd9c117c97ff6254c
hexpeetols10 <Petersetolulope21@gmail.com>
2025-06-14 04:37:28 +0100

    Food hub

---
+++ Fireball.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
---
+++ Script.js
@@ -0,0 +1,3 @@
+function orderNow() {
+  alert("Thanks for ordering!");
+}
